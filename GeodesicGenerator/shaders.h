3ce5a622bd53c4230097a6d1a563849a9e95c391
Sam Kingdon <smarky55@gmail.com>
2017-07-01 21:17:40 +0100

    Shader errors now print a small section of the code to ease IDing errors

--- GeodesicGenerator/shaders.h
+++ GeodesicGenerator/shaders.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <regex>
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
@@ -15,8 +16,9 @@ GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_pat
 class ShaderProgram {
 	std::vector<GLuint> shaderStages;
 	bool hasLinked = false;
-	void loadFile(const char * file_path, std::string& code);
+	void loadFile(const char * file_path, std::string& code, std::vector<std::string>& lines);
 	const char * stageName(GLenum stage);
+	void printErrors(GLenum stage, std::vector<char>& errorMessage, std::vector<std::string>& lines);
 public:
 	GLint programID;
 	ShaderProgram();
