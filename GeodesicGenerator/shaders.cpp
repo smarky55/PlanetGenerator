03e8e29b341accaad9416beacdd59b469261698d
Sam Kingdon <smarky55@gmail.com>
2017-07-02 23:13:43 +0100

    Imporved shader error reporting

--- GeodesicGenerator/shaders.cpp
+++ GeodesicGenerator/shaders.cpp
@@ -1,6 +1,6 @@
 #include "shaders.h"
 
-void ShaderProgram::loadFile(const char * file_path, std::string & code) {
+void ShaderProgram::loadFile(const char * file_path, std::string & code, std::vector<std::string>& lines) {
 	std::ifstream stream(file_path, std::ios::in);
 	code = "";
 	if(stream.is_open()) {
@@ -11,11 +11,14 @@ void ShaderProgram::loadFile(const char * file_path, std::string & code) {
 				size_t gt_pos, lt_pos;
 				gt_pos = line.find("<", include_pos + 8);
 				lt_pos = line.find(">", gt_pos);
-				loadFile(line.substr(gt_pos + 1, lt_pos - (gt_pos + 1)).c_str(), line);
+				std::vector<std::string> includeLines;
+				loadFile(line.substr(gt_pos + 1, lt_pos - (gt_pos + 1)).c_str(), line, includeLines);
 				code += line;
+				lines.insert(lines.end(), includeLines.begin(), includeLines.end());
 			} else {
 				//std::cout << line << std::endl;
 				code += line + "\n";
+				lines.push_back(line);
 			}
 		}
 		stream.close();
@@ -37,6 +40,26 @@ const char * ShaderProgram::stageName(GLenum stage) {
 	}
 }
 
+void ShaderProgram::printErrors(GLenum stage, std::vector<char>& errorMessage, std::vector<std::string>& lines) {
+	std::string errors = std::string(errorMessage.begin(), errorMessage.end());
+	std::regex r1("\\(\\d+\\)");
+	std::regex r2("\\d\\(\\d+\\)[^\n]*");
+	std::sregex_iterator it1(errors.begin(), errors.end(), r1);
+	std::sregex_iterator it2(errors.begin(), errors.end(), r2);
+	std::sregex_iterator it_end;
+	printf("%s Errors:\n", stageName(stage));
+	while(it1 != it_end && it2 != it_end) {
+		size_t index = stoi(errors.substr((*it1).position() + 1, (*it1).length() - 2)) - 1;
+		std::cout << (*it2).str() << std::endl;
+		for(auto i = lines.begin() + (index - 2); i != lines.end() && i != lines.begin() + (index + 2); i++) {
+			std::cout << std::distance(lines.begin(), i) + 1 << *i << std::endl;
+
+		}
+		it1++;
+		it2++;
+	}
+}
+
 ShaderProgram::ShaderProgram() {
 	programID = glCreateProgram();
 }
@@ -50,8 +73,8 @@ void ShaderProgram::addStage(GLenum stage, const char * file_path) {
 		GLuint shaderID = glCreateShader(stage);
 
 		std::string shaderCode;
-
-		loadFile(file_path, shaderCode);
+		std::vector<std::string> shaderLines;
+		loadFile(file_path, shaderCode, shaderLines);
 
 		const char * source = shaderCode.c_str();
 		glShaderSource(shaderID, 1, &source, NULL);
@@ -64,7 +87,8 @@ void ShaderProgram::addStage(GLenum stage, const char * file_path) {
 		if(result == GL_FALSE && infoLogLength > 0) {
 			std::vector<char> errorMessage(infoLogLength + 1);
 			glGetShaderInfoLog(shaderID, infoLogLength, NULL, &errorMessage[0]);
-			printf("%s Errors:\n%s\n", stageName(stage), &errorMessage[0]);
+			//printf("%s Errors:\n%s\n", stageName(stage), &errorMessage[0]);
+			printErrors(stage, errorMessage, shaderLines);
 			throw std::runtime_error("Error when compiling shader");
 		}
 
@@ -92,8 +116,6 @@ void ShaderProgram::linkProgram() {
 			printf("Program Errors:\n%s\n", &errorMessage[0]);
 			throw std::runtime_error("Error when linking shader");
 		}
-
-		hasLinked = true;
 
 		for each (GLuint shaderID in shaderStages) {
 			glDeleteShader(shaderID);
--- GeodesicGenerator/shaders.h
+++ GeodesicGenerator/shaders.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <regex>
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
@@ -11,13 +12,14 @@
 class ShaderProgram {
 	std::vector<GLuint> shaderStages;
 	bool hasLinked = false;
-	void loadFile(const char * file_path, std::string& code);
+	void loadFile(const char * file_path, std::string& code, std::vector<std::string>& lines);
 	const char * stageName(GLenum stage);
+	void printErrors(GLenum stage, std::vector<char>& errorMessage, std::vector<std::string>& lines);
 public:
 	GLint programID;
 	ShaderProgram();
 	~ShaderProgram();
-	bool isLinked() { return hasLinked; }
+
 	void addStage(GLenum stage, const char * file_path);
 	void linkProgram();
 };
